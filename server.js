9fac6b3438a4e30938cadf71f350abbb9629218d
Heinz Araque <araqueheinz@gmail.com>
2019-09-09 08:56:53 -0400

    inital server setup and dependendies added

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Api Running');
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`));
